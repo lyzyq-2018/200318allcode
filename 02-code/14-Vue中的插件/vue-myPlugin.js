1ce784ba2ba722a5b8654c071124ce3393c4f5e3
24kcs <younghongbo@126.com>
2020-06-29 17:04:42 +0800

    2codes

--- 02-code/14-Vue中的插件/vue-myPlugin.js
+++ 02-code/14-Vue中的插件/vue-myPlugin.js
@@ -1,38 +1,25 @@
-// (function (window) {
-// 	const myPlugin = {}
-// 	// 暴露该方法
-// 	myPlugin.install = function (Vue) {
-// 		// 添加一个全局方法
-// 		Vue.myGlobalMethod = function () {
-// 			console.log('全局方法调用')
-// 		}
-// 		// 添加一个全局的资源--指令
-// 		Vue.directive('my-directive', (el, binding) => {
-// 			el.textContent = 'my-directive' + '======' + binding.value
-// 		})
-// 		// 添加一个实例方法
-// 		Vue.prototype.$myMethod = function () {
-// 			console.log('实例对象调用')
-// 		}
-// 	}
-// 	window.myPlugin = myPlugin
-// })(window)
-
+// Vue中自定义插件的思路
 (function (window) {
-	// 定义对象
+	// 插进一般都会暴露一个插件对象
+	// 定义插件对象
 	const MyPlugin = {}
+
 	MyPlugin.install = function (Vue) {
+		// 1. 添加全局方法或 property
 		Vue.myGlobalMethod = function () {
-			console.log('全局方法')
+			// 逻辑...
+			console.log('我是一个全局方法')
 		}
-		// 添加一个全局的资源指令
+		// 2. 添加全局资源
 		Vue.directive('my-directive', (el, binding) => {
-			el.textContent = 'my-directive' + '======' + binding.value
+			el.textContent = binding.value + '====哈哈'
 		})
-		// 添加一个实例方法
-		Vue.prototype.$myMethod = function () {
-			console.log('实例对象方法')
+		// 3. 添加实例方法
+		Vue.prototype.$myMethod = function (methodOptions) {
+			// 逻辑...
+			console.log('我是一个实例方法')
 		}
 	}
+	// 暴露该对象
 	window.MyPlugin = MyPlugin
 })(window)
