0b0bf441ae8dae89104c4d8bad519dec06b4f226
24kcs <younghongbo@126.com>
2020-06-30 11:53:19 +0800

    3codes

---
+++ 03-code/app/src/components/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+<script>
+// 引入Item 组件
+import Item from './Item'
+export default {
+  name: 'List',
+  components: {
+    Item
+  }
+}
+</script>
+<style scoped>
+</style>
