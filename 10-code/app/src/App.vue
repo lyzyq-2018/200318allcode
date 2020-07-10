8e65d2858afb9a392dbed0d19e85da14777389b3
24kcs <younghongbo@126.com>
2020-07-10 16:58:02 +0800

    10codes

--- 10-code/app/src/App.vue
+++ 10-code/app/src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="navContainer">
+      <div class="nav">
+        <router-link to="/home">Home路由链接</router-link>
+      </div>
+      <div class="nav">
+        <!-- <router-link to="/about">About路由链接</router-link> -->
+        <!--params的方式传参-->
+        <!-- <router-link to="/about/123">About路由链接</router-link> -->
+        <!--query的方式传参-->
+        <!-- <router-link to="/about?id=100">About路由链接</router-link> -->
+        <!--meta的方式传参-->
+        <!-- <router-link to="/about">About路由链接</router-link> -->
+        <!--props的布尔方式-->
+        <!-- <router-link to="/about">About路由链接</router-link> -->
+        <!--props的对象模式-->
+        <!-- <router-link to="/about">About路由链接</router-link> -->
+        <!--props的函数模式-->
+        <router-link to="/about">About路由链接</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+    <hr />
+    <!-- <p>{{$route.params.id}}</p>
+    <p>{{$route.query.id}}</p>
+    <p>{{$route.meta.showMsg}}</p>-->
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+.navContainer
+  width 100%
+  height 80px
+  display flex
+  .nav
+    width 50%
+    height 80px
+    line-height 80px
+    text-align center
+    border-bottom 2px solid green
 </style>
--- 10-code/第十天笔记.js
+++ 10-code/第十天笔记.js
@@ -8,15 +8,6 @@
  * 
  */
 
-
-
-
-
-
-
-
-
-
  
 /**
  * 
@@ -47,17 +38,96 @@
  * serve dist
  * 
  * 
+ * 脚手架2和脚手架3的区别:
+ * 1. 目录的文件个数不同
+ * 2. 脚手架2中有static目录,用来存储静态资源的,脚手架3中没有这个目录,静态资源可以放在public目录中
+ * 3. 在浏览器中自动打开的设置不同
+ *    脚手架2在config目录中的index.js文件中18行的位置设置为true,则可以在浏览器中自动的打开
+ *    脚手架3中需要在package.json中第6行位置,需要加个--open,即可在浏览器中自动的打开
+ * 4. eslint的设置不同
+ *    脚手架2中在.eslintignore文件中可以直接设置哪些文件不用eslint检查
+ *    脚手架2中在.eslintrc.js文件中可以逐个设置eslint语法检查是否关闭
+ *    脚手架3中可以在vue.config.js文件中通过lintOnSave设置布尔类型的值 进行eslint语法检查的设置
+ *    vue.config.js文件需要手动的添加到根目录中
+ * 5. main.js中的代码不同
+ *    脚手架3中的代码和脚手架2的不一样
+ * 6. 跨域配置的设置不同----
+ * 
+ * 
+ * 在脚手架中如果想要使用stylus 书写样式,首先需要安装stylus的相关插件
+ *  npm install stylus stylus-loader
  * 
  * 
  * 
- * 脚手架2中浏览器自动打开的设置,config 目录中index.js 18行代码设置为true
- * 脚手架3中浏览器自动打开的设置,package.json 中 进行设置  --open即可
  * 
- * 脚手架2和脚手架3的目录的区别
- * public公共资源及index.html的目录
- * src目录中的main.js代码不一样
  * 
- * 脚手架2和脚手架3的eslint检查设置也不同
+ * 路由传参的方式:
+ * 1.params的方式:
+ *  在注册路由的时候,需要使用:进行占位,在组件内部可以通过$route路由信息对象.params.xxx的方式获取路由传递的参数数据
+ *  如:注册路由的时候
+ *   {path:'/about/:id',component:About}
+ *   组件中如何获取参数数据
+ *   $route.params.id 即可获取
+ *  params的方式传参,很明显想要获取数据离不开路由信息对象$route(是需要当前组件实例对象来获取的this.$route.params.id)
+ *  params的传参方式和组件的联系很密切----耦合度比较高
+ * 
+ * 2.query的方式:
+ *  在注册路由的时候,是不需要进行占位的,组件内部可以通过$route路由信息对象.query.xxx的方式获取路由传递的参数数据
+ *  如: 注册路由和平时一样
+ *   {path:'/about',component:About}
+ *  组件中如何获取参数数据
+ *  $route.query.id 即可获取
+ *  query的方式传参,很明显想要获取数据离不开路由信息对象$route(是需要当前组件实例对象来获取的this.$route.query.id)
+ *  query的传参方式和组件的联系也很密切---耦合度比较高
+ * 
+ * 3.meta的方式:
+ *  在注册路由的时候,需要在组件内部设置meta对象,内部可以书写任何的键值对,组件内部可以通过$route路由信息对象.meta.xxx的方式获取路由传递的参数数据
+ *  如:{path:'/about',component:About,meta:{showMsg:true}}
+ *  组件中如何获取参数数据
+ *  $route.meta.showMsg 即可获取
+ * meta的方式传参,很明显想要获取数据离不开路由信息对象$route(是需要当前组件实例对象来获取的this.$route.meta.xxx)
+ * meta的传参方式和组件的联系也很密切---耦合度比较高
+ * 
+ * 总结:params传参和query方式传参和meta方式传参,和组件的耦合度都是很高(以上三种方式传参及获取,离不开组件对象),但是不是很灵活.(组件和$route的关系很密切,高度耦合)
+ * props的方式让组件和$route的关系不那么密切了,解耦
+ * 
+ * 
+ * 4.props的方式:
+ *  布尔模式: 先在注册路由的时候,使用props并设置为true,则需要在组件内部通过props的方式接收参数,即可使用传递的参数数据
+ * 如: {path:'/about/:id',component:About,props:true}
+ * 组件内部:props:['id'] 直接可以使用id来获取参数数据
+ * 可以不用$route,此时的组件和$route进行了解耦
+ * 
+ *  对象模式: 先在注册路由的时候,使用props的对象的方式,则需要在组件内部通过props的方式接收参数,即可使用传递的参数数据
+ * 如: {path:'/about/:id',component:About,props:{msg:'真香'}}
+ * 组件内部:props:['msg'] 直接可以使用msg来获取参数数据
+ * 可以不用$route,此时的组件和$route进行了解耦
+ * 
+ *  函数模式:先在注册路由的时候,使用props的函数模式的方式,则需要在组件内部通过props的方式接收参数,即可使用传递的参数数据
+ * 如: {path:'/about',component:About,props:route=>({txt:'真好'})}
+ * 组件内部:props:['txt'] 直接可以使用txt来获取参数数据
+ * 可以不用$route,此时的组件和$route进行了解耦
+ * 
+ * 总结:
+ *  路由传参,可以使用params的方式,也可以使用query的方式,还可以使用meta的方式,都可以使用props的方式进行解耦操作
+ * 
+ * 
+ *  
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  * 
  * 
  * 
